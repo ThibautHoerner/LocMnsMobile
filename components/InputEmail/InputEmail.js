d65f13a8424f9ae648a6a59cb05b7708a8446e78
robin trichot <robintrichot93@gmail.com>
2023-06-22 11:30:44 +0200

    verifEmail

--- components/InputEmail/InputEmail.js
+++ components/InputEmail/InputEmail.js
@@ -6,6 +6,20 @@ import InputEmailStyles from "./InputEmailStyles";
 export default InputEmail = ({ control, errors }) => {
   const styles = { ...AppStyles(), ...InputEmailStyles() };
 
+  const utilisateurs = [
+    { id: 1, email: "Admin@a.com", password: "1234", },
+    { id: 2, email: "Rambo@r.com", password: "1967", },
+    { id: 3, email: "User@u.som", password: "kikou", },
+  ];
+
+  const validateEmail = (value) => {
+    const isemailValid = utilisateurs.some((utilisateur) => utilisateur.email === value);
+    if (!isemailValid) {
+      return "Veuillez sélectionner un email valide.";
+    }
+    return true;
+  };
+
   return (
     <Controller
       control={control}
@@ -29,6 +43,7 @@ export default InputEmail = ({ control, errors }) => {
           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
           message: "Veuillez entrer une adresse email valide.",
         },
+        validate: validateEmail,
       }}
       defaultValue=""
     />
--- screens/Connexion/Connexion.js
+++ screens/Connexion/Connexion.js
@@ -2,19 +2,27 @@ import ConnexionStyles from "./ConnexionStyles";
 import AppStyles from "../../AppStyles";
 import { Button, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { TextInput } from "react-native-gesture-handler";
+import { useForm } from "react-hook-form";
 
 export default ({ handleLogin }) => {
     const styles = { ...AppStyles(), ...ConnexionStyles() };
 
-    const navigation = useNavigation();
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        getValues,
+    } = useForm();
 
     return (
         <View style={[styles.container, styles.centered, styles.safeArea]}>
-            <Button onPress={handleLogin} title="CONNEXION"></Button>
-            <Button
-                onPress={() => navigation.navigate("Inscription")}
-                title="Inscription"
-            ></Button>
+            
+            <InputEmail control={control} errors={errors}></InputEmail>
+            <InputPassword control={control} errors={errors}></InputPassword>
+
+            <Button title="CONNEXION" onPress={handleSubmit(handleLogin)} />
+          
         </View>
     );
 };
